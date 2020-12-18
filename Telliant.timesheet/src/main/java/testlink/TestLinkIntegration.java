06385c5a813980fe62a9518fbb3aceaecb17287b
vicky <vicky@DESKTOP-N727QKO>
2020-12-18 12:53:14 +0530

    comit

--- Telliant.timesheet/src/main/java/testlink/TestLinkIntegration.java
+++ Telliant.timesheet/src/main/java/testlink/TestLinkIntegration.java
@@ -5,11 +5,11 @@ import utilities.PropertyFile;
 
 public class TestLinkIntegration {
 
-public static void updateResults(String testCase, String exception, String result) throws TestLinkAPIException {
+public static void updateResults(String Testcase_Name, String exception, String result, String notes) throws TestLinkAPIException {
     TestLinkAPIClient testlinkAPIClient = new TestLinkAPIClient(PropertyFile.prop.getProperty("APIKEY"), PropertyFile.prop.getProperty("Testlink_URL"));
     testlinkAPIClient.reportTestCaseResult(PropertyFile.prop.getProperty("project_Name"),
     		PropertyFile.prop.getProperty("Test_Plan"),PropertyFile.prop.getProperty("Testcase_Name"),     		
-    		PropertyFile.prop.getProperty("Build_Name"), exception, result);
+    		PropertyFile.prop.getProperty("Build_Name"), exception, result,notes);
 
 }
 
--- Telliant.timesheet/src/test/java/Telliant_timesheet/app/LoginTest.java
+++ Telliant.timesheet/src/test/java/Telliant_timesheet/app/LoginTest.java
@@ -1,9 +1,14 @@
  package Telliant_timesheet.app;
 
+import java.lang.reflect.Method;
+
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Test;
 
 import baseclass.test;
@@ -18,6 +23,8 @@ import testlink.api.java.client.TestLinkAPIResults;
 public class LoginTest extends test {
 	public static String Actual;
 	public static String Expected;
+	static String notes = null;
+	static String testresult = null;
     HomePage homePage = PageFactory.initElements(driver, HomePage.class);
     LoginPage loginpage = PageFactory.initElements(driver, LoginPage.class);
 	TimesheetPage timeSheetPage = PageFactory.initElements(driver, TimesheetPage.class);
@@ -30,19 +37,17 @@ public class LoginTest extends test {
 
 	@Test(priority = 1, testName = "test1", description = "LoginTimesheet")
 	public void login() throws TestLinkAPIException {
-	
-		loginpage.enterUserName(PropertyFile.prop.getProperty("Myusername"));
-		loginpage.enterPassword(PropertyFile.prop.getProperty("Mypassword"));
-		loginpage.submitLogin();
-		Actual = loginpage.validateLoginScreen();
-		Expected = PropertyFile.prop.getProperty("ExpectedLoggedUser");
-		Assert.assertEquals(Actual, Expected);
-		try {TestLinkIntegration.updateResults("DP-59",null,TestLinkAPIResults.TEST_PASSED);
-		 
-		}
-		catch(Exception e){
-			TestLinkIntegration.updateResults("DP-59",e.getMessage(),TestLinkAPIResults.TEST_FAILED);
-					}
+		
+		
+		
+			loginpage.enterUserName(PropertyFile.prop.getProperty("Myusername"));
+			loginpage.enterPassword(PropertyFile.prop.getProperty("Mypassword"));
+			loginpage.submitLogin();
+			Actual = loginpage.validateLoginScreen();
+			Expected = PropertyFile.prop.getProperty("ExpectedLoggedUser");
+			Assert.assertEquals(Actual, Expected);
+			
+		
 		
 	}
 
@@ -53,25 +58,61 @@ public class LoginTest extends test {
 		Actual = homePage.validateViewTimeSheetpage();
 		Expected = PropertyFile.prop.getProperty("ExpectedURl");
 		Assert.assertEquals(Actual, Expected);
-		try {TestLinkIntegration.updateResults("DP-61",null,TestLinkAPIResults.TEST_PASSED);
-		}
-		catch(Exception e){
-			TestLinkIntegration.updateResults("DP-61",e.getMessage(),TestLinkAPIResults.TEST_FAILED);
-		}
-	}
-
-	@Test(priority = 3, testName = "test3", description = "Clear existing actions")
-	public void clearExistingAction() throws Exception {
-
-		timeSheetPage.clearExistingProjects();
-		//timeSheetPage.addProject();
-	try {TestLinkIntegration.updateResults("DP-62",null,TestLinkAPIResults.TEST_PASSED);
-
-	}
-		catch(Exception e){
-			TestLinkIntegration.updateResults("DP-62",e.getMessage(),TestLinkAPIResults.TEST_FAILED);
-		}
-
 		
+		}
+		@AfterMethod
+
+		public static void writeResult(ITestResult result) throws Exception {
+			try {
+				if (result.getStatus() == ITestResult.SUCCESS) {
+
+					notes = "Executed successfully-from jenkins";
+					testresult = TestLinkAPIResults.TEST_PASSED;
+					// testlink_configuration.updateResults(TestcaseID, null,testresult, notes);
+				}
+
+				else if (result.getStatus() == ITestResult.FAILURE) {
+
+					System.out.println("Log Message:: @AfterMethod: Method-" + "- has Failed");
+					notes = "Execution Failedfrom jenkins";
+					testresult = TestLinkAPIResults.TEST_FAILED;
+					// testlink_configuration.updateResults("testcaseID, null,testresult, notes);
+					
+
+				}
+			} catch (Exception e) {
+				System.out.println("\nLog Message::@AfterMethod: Exception caught");
+				notes = "Caught new Expection";
+				
+				testresult = TestLinkAPIResults.TEST_FAILED;
+				// testlink_configuration.updateResults("testcaseID, null,testresult, notes);
+				
+			} finally {
+
+				TestLinkIntegration.updateResults(result.getMethod().getDescription(), null, testresult, notes);
+			}
+
+		}
+		 @BeforeMethod
+		    public void testSetup(@Optional Method method) throws Exception {
+		        String description = method.getAnnotation(Test.class).description();
+		    System.out.println(""+description);
+		    }
 	}
-}
+//
+//	@Test(priority = 3, testName = "test3", description = "Clear existing actions")
+//	public void clearExistingAction() throws Exception {
+//
+//		
+//		
+//	try {
+//		TestLinkIntegration.updateResults("DP-59",null,TestLinkAPIResults.TEST_PASSED);
+//	timeSheetPage.clearExistingProjects();
+//	}
+//		catch(Exception e){
+//			TestLinkIntegration.updateResults("DP-59",e.getMessage(),TestLinkAPIResults.TEST_FAILED);
+//		}
+//
+//		
+//	}
+//}
